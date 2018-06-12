be9d31934b0cc16f454711f1d1a069e1cb110514
ikalchenko <ikalchenko99@gmail.com>
2018-06-12 13:28:38 +0300

    add some templates

--- komitets/models.py
+++ komitets/models.py
@@ -1,17 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    password = models.CharField(max_length=50)
+# class User(models.Model):
+#     username = models.CharField(max_length=50, unique=True)
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     email = models.EmailField()
 
 
-class Komitet(models.Model):
+class Committee(models.Model):
     name = models.CharField(max_length=50)
     access_code = models.CharField(max_length=10)
+    access_confirmation = models.BooleanField()
+    created = models.DateTimeField()
 
+
+class Question(models.Model):
+    committee = models.ForeignKey(Committee, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    CARD_TYPES = (
+        ('announcement', 'announcement'),
+        ('payment', 'payment'),
+        ('assignment', 'assignment'),
+        ('yes-no-poll', 'yes-no-poll'),
+        ('multi-poll', 'multi-poll'),
+    )
+    card_type = models.CharField(max_length=15, choices=CARD_TYPES)
+    created = models.DateTimeField()
+
+
+class QuestionAnswers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=50)
+
+
+class UserPermissions(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    committee = models.ForeignKey(Committee, on_delete=models.CASCADE)
+    PERMISSIONS = (
+        ('a', 'a'),
+        ('rw', 'rw'),
+        ('r', 'r'),
+        ('b', 'b'),
+    )
+    permission = models.CharField(max_length=2, choices=PERMISSIONS)
--- komitets/urls.py
+++ komitets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
+app_name = 'komitets'
 urlpatterns = [
     path('', views.TestView.as_view(), name='test'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
