d1e36eee17e525a994d0712c83e9ad7deac9683e
Ilya Kalchenko <ikalchenko99@gmail.com>
2018-06-18 12:38:36 +0300

    create formset for inviting users to komitet

---
+++ komitet/local.py
@@ -0,0 +1,18 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'i.am.kalchenko@gmail.com'
+EMAIL_HOST_PASSWORD = '99011qwevbn'
+EMAIL_PORT = 587
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'komitet',
+        'USER': 'ik',
+        'PASSWORD': '1234pass',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
--- komitets/forms.py
+++ komitets/forms.py
@@ -3,3 +3,12 @@ from django import forms
 
 class CreateKomitetForm(forms.Form):
     name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', }))
+
+
+class InviteUserToKomitetForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control', })
+    )
+
+
+InviteUserFormSet = forms.formset_factory(InviteUserToKomitetForm)
